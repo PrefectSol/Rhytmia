fae25eba513c74a2b7ac327316b8707c57448545
Prefect <prefectsol@mail.ru>
2023-11-04 15:14:30 +0500

    Add recent_play pre-plugin\

---
+++ recent_plays/recent_plays.cpp
@@ -0,0 +1,28 @@
+﻿// recent_plays.cpp : Определяет экспортируемые функции для DLL.
+//
+
+#include <SDL.h>
+
+#include "pch.h"
+#include "framework.h"
+#include "recent_plays.h"
+
+
+
+extern "C" RECENTPLAYS_API void DrawRectangle(SDL_Window * window) {
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cerr << "Failed to create SDL2 renderer: " << SDL_GetError() << std::endl;
+        return;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Красный цвет
+    SDL_RenderClear(renderer);
+
+    SDL_Rect rect = { 100, 100, 200, 200 }; // Координаты и размеры прямоугольника
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255); // Зеленый цвет
+    SDL_RenderFillRect(renderer, &rect);
+
+    SDL_RenderPresent(renderer);
+    SDL_DestroyRenderer(renderer);
+}
---
+++ recent_plays/recent_plays.h
@@ -0,0 +1,22 @@
+﻿// Приведенный ниже блок ifdef — это стандартный метод создания макросов, упрощающий процедуру
+// экспорта из библиотек DLL. Все файлы данной DLL скомпилированы с использованием символа RECENTPLAYS_EXPORTS
+// Символ, определенный в командной строке. Этот символ не должен быть определен в каком-либо проекте,
+// использующем данную DLL. Благодаря этому любой другой проект, исходные файлы которого включают данный файл, видит
+// функции RECENTPLAYS_API как импортированные из DLL, тогда как данная DLL видит символы,
+// определяемые данным макросом, как экспортированные.
+#ifdef RECENTPLAYS_EXPORTS
+#define RECENTPLAYS_API __declspec(dllexport)
+#else
+#define RECENTPLAYS_API __declspec(dllimport)
+#endif
+
+// Этот класс экспортирован из библиотеки DLL
+class RECENTPLAYS_API Crecentplays {
+public:
+	Crecentplays(void);
+	// TODO: добавьте сюда свои методы.
+};
+
+extern RECENTPLAYS_API int nrecentplays;
+
+RECENTPLAYS_API int fnrecentplays(void);
